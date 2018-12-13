4be6dc7556b4b45e09854e9d0c83f84f936c269c
vix95 <38139321+vix95@users.noreply.github.com>
2018-12-13 21:42:25 +0100

    change source

--- 2018/day_1/day_1/main.c
+++ 2018/day_1/day_1/main.c
@@ -1,25 +1,31 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define ARRAY_SIZE 10
 
 int main() {
 	int freq = 0;
-	int x;
-	fstream f;
+	FILE *f;
 
-	f.open("input.txt");
-	if (!f) {
-		cout << "Unable to open file";
-		cin.get();
-		exit(1);
+	f = fopen("input.txt", "r");
+	if (f == NULL) {
+		printf("Unable to open file");
+		return 1;
 	}
 
-	while (f >> x) {
-		freq = freq + x;
+	char line[ARRAY_SIZE];
+	while (fgets(line, sizeof line, f) != NULL) {
+		int num = 0;
+		for (int i = 1; i < ARRAY_SIZE; i++) {
+			if (line[i] == '\n') break;
+			num = num * 10 + (line[i] - 48);
+		}
+
+		if (line[0] == '+') freq += num;
+		if (line[0] == '-') freq -= num;
 	}
 
-	f.close();
-	cout << freq << endl;
-	cin.get();
+	fclose(f);
+	printf("%d", freq);
 	return 0;
 }
--- 2018/day_2/day_2/main.c
+++ 2018/day_2/day_2/main.c
@@ -1,82 +1,72 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-const int LINES = 250;
-const int ARRAY_SIZE = 26;
+#define LINES 250
+#define ARRAY_SIZE 28
 
-void addCounters(int arr1[], char arr2[][ARRAY_SIZE], int &a, int &b);
+void addCounters(char arr[], int *a, int *b);
 int calcCheckSum(int a, int b);
-void getCommonLetters(char letters[], char words[][ARRAY_SIZE]);
+void getCommonLetters(char letters[], char org[], char compare[]);
 void printCommonLetters(char arr[]);
 
 int main() {
-	int letters[ARRAY_SIZE] = { 0 };
-	char words[LINES][ARRAY_SIZE] = {};
+	char words[LINES][ARRAY_SIZE];
 	int line = 0;
 	int two = 0;
 	int three = 0;
-	fstream f;
+	FILE *f;
 
-	f.open("input.txt");
-	if (!f) {
-		cout << "Unable to open file";
-		cin.get();
-		exit(1);
+	f = fopen("input.txt", "r");
+	if (f == NULL) {
+		printf("Unable to open file");
+		return 1;
 	}
 
-	while (!f.eof()) {
-		int i = 0;
-		char c;
+	while (fgets(words[line++], sizeof words[line], f) != NULL);
+	fclose(f);
 
-		f.get(c);
-		while (c != '\n') {
-			words[line][i] = c;
-			i++;
-			f.get(c);
+	for (int i = 0; i < LINES; i++) {
+		addCounters(words[i], &two, &three);
+	}
+
+	printf("%d\n", calcCheckSum(two, three));
+
+	char commonLetters[ARRAY_SIZE];
+	for (int i = 0; i < LINES; i++) {
+		for (int j = 0; j < LINES; j++) {
+			getCommonLetters(&commonLetters, words[i], words[j]);
 		}
-
-		line++;
 	}
 
-	f.close();
-
-	addCounters(letters, words, two, three);
-	cout << calcCheckSum(two, three) << endl;
-
-	char commonLetters[ARRAY_SIZE] = {};
-	getCommonLetters(commonLetters, words);
 	printCommonLetters(commonLetters);
 
-	cin.get();
 	return 0;
 }
 
-void addCounters(int arr1[], char arr2[][ARRAY_SIZE], int &a, int &b) {
-	for (int i = 0; i < LINES; i++) {
-		bool two = false;
-		bool three = false;
-		int chars[ARRAY_SIZE] = { 0 };
+void addCounters(char arr[], int *a, int *b) {
+	int two = 0;
+	int three = 0;
+	int chars[ARRAY_SIZE] = { 0 };
 
-		for (int j = 0; j < ARRAY_SIZE; j++) {
-			chars[arr2[i][j] - 97]++;
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		if (arr[i] == '\n') break;
+		chars[arr[i] - 97]++;
+	}
+
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		if (arr[i] == '\n') break;
+
+		if (!two && chars[i] == 2) {
+			*a = *a + 1;
+			two = 1;
 		}
 
-		for (int j = 0; j < ARRAY_SIZE; j++) {
-			if (!two && chars[j] == 2) {
-				arr1[j]++;
-				a++;
-				two = true;
-			}
-
-			if (!three && chars[j] == 3) {
-				arr1[j]++;
-				b++;
-				three = true;
-			}
-
-			if (two && three) break;
+		if (!three && chars[i] == 3) {
+			*b = *b + 1;
+			three = 1;
 		}
+
+		if (two && three) break;
 	}
 }
 
@@ -84,28 +74,24 @@ int calcCheckSum(int a, int b) {
 	return a * b;
 }
 
-void getCommonLetters(char letters[], char words[][ARRAY_SIZE]) {
-	for (int i = 0; i < LINES; i++) {
-		for (int j = 0; j < LINES; j++) {
-			int commonCount = 0;
-			int uncommonIndex = 0;
+void getCommonLetters(char letters[], char org[], char compare[]) {
+	int commonCount = 0;
+	int uncommonIndex = 0;
 
-			for (int k = 0; k < ARRAY_SIZE; k++) {
-				if (words[i][k] == words[j][k]) {
-					commonCount++;
-				}
-				else {
-					uncommonIndex = k;
-				}
-			}
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		if (org[i] == compare[i]) {
+			commonCount++;
+		}
+		else {
+			uncommonIndex = i;
+		}
+	}
 
-			if (commonCount == ARRAY_SIZE - 1) {
-				int pos = 0;
-				for (int k = 0; k < ARRAY_SIZE; k++) {
-					if (k != uncommonIndex) {
-						letters[pos++] = words[i][k];
-					}
-				}
+	if (commonCount == ARRAY_SIZE - 1) {
+		int pos = 0;
+		for (int j = 0; j < ARRAY_SIZE; j++) {
+			if (j != uncommonIndex) {
+				letters[pos++] = org[j];
 			}
 		}
 	}
@@ -113,6 +99,7 @@ void getCommonLetters(char letters[], char words[][ARRAY_SIZE]) {
 
 void printCommonLetters(char arr[]) {
 	for (int i = 0; i < ARRAY_SIZE; i++) {
-		cout << arr[i];
+		if (arr[i] == '\n') return;
+		printf("%c", arr[i]);
 	}
 }
